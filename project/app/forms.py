91e5d351c36a70442d1ddc28f631fc12e852db27
Alexandra <alexandracarvalho@ua.pt>
2021-04-26 19:30:45 +0100

    Search bar layout

--- project/app/forms.py
+++ project/app/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search for an image...'}))
+    query = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search for an image...'}), max_length=100)
