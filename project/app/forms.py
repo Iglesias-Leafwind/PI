8d7a1e190ad45cfb0b6402cca469e141fb4aae3d
Alexandra <alexandracarvalho@ua.pt>
2021-04-29 23:43:16 +0100

    First and second models downgrades

--- project/app/forms.py
+++ project/app/forms.py
@@ -1,4 +1,7 @@
+import os
+
 from django import forms
+from django.conf import settings
 
 
 class SearchForm(forms.Form):
@@ -7,3 +10,8 @@ class SearchForm(forms.Form):
 
 class SearchForImageForm(forms.Form):
     image = forms.ImageField(label="", required=False)
+
+
+class EditFoldersForm(forms.Form):
+    path = forms.FilePathField(label="", path=os.getenv("HOME"), required=False)
+
--- project/app/views.py
+++ project/app/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render
-from app.forms import SearchForm, SearchForImageForm
+from app.forms import SearchForm, SearchForImageForm, EditFoldersForm
 
 def index(request):
+    folders = []  # folders should be Folder.objects.all()
+
     if request.method == 'POST':
         query = SearchForm(request.POST)
         image = SearchForImageForm(request.POST, request.FILES)
-        if query.is_valid() and query.cleaned_data['query']:
+        pathf = EditFoldersForm(request.POST)
+        results = []
+        if query.is_valid() and query.cleaned_data['query']: # search bar input was valid and not null
             query_array = query.cleaned_data['query'].split(" ")
             # use the query_array to search by tags and pass them as 'results'
             query = SearchForm()
-            return render(request, "index.html", {'form': query, 'image_form': image, 'results': {}})
-        elif image.is_valid():
+            return render(request, "index.html", {'form': query, 'image_form': image, 'path_form': pathf, 'folders': folders, 'results': results}) #return new index with results this time and cleaned form
+        elif image.is_valid() and image.cleaned_data["image"]: # if search by image file exists
             image = image.cleaned_data["image"]
             # use the image to process image and look for similar images in model
             image = SearchForImageForm()
-            return render(request, "index.html", {'form': query, 'image_form': image, 'results' : {}})
-        else:
+            return render(request, "index.html", {'form': query, 'image_form': image,  'path_form': pathf, 'folders': folders, 'results': results}) #return new index with results this time and cleaned form
+        elif pathf.is_valid() and pathf.cleaned_data["path"]: # if path of new folder has a name, then it exists
+            # new = Folder(url = pathf.path)
+            # new.save()
+            pathf = EditFoldersForm()
+            return render(request, "index.html", {'form': query, 'image_form': image, 'path_form': pathf, 'folders': folders,'results': results})  # return new index with results this time and cleaned form
+        else: # the form filled had a mistake
             form = SearchForm()
             image = SearchForImageForm()
-            return render(request, 'index.html', {'form': form, 'image_form': image, 'error': True})
-    else:
+            pathf = EditFoldersForm()
+            return render(request, 'index.html', {'form': form, 'image_form': image,  'path_form': pathf, 'folders': folders, 'results': []})
+
+    else:   # first time in the page - no forms filled
         form = SearchForm()
         image = SearchForImageForm()
-        return render(request, 'index.html', {'form': form, 'image_form': image, 'error': False})
+        path = EditFoldersForm()
+        return render(request, 'index.html', {'form': form, 'image_form': image,  'path_form': path, 'folders': folders, 'results': []})
