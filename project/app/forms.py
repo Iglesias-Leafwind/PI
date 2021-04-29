a036ffc4274f78c66a5bec39ae23090f51e9dce0
Alexandra <alexandracarvalho@ua.pt>
2021-04-29 17:09:57 +0100

    First modal (add image) skeleton

--- project/app/forms.py
+++ project/app/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search for an image...'}), max_length=100)
+    query = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search for an image...'}), max_length=100, required=False)
+
+
+class SearchForImageForm(forms.Form):
+    image = forms.ImageField(label="", required=False)
--- project/app/views.py
+++ project/app/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from app.forms import SearchForm
+from app.forms import SearchForm, SearchForImageForm
 
 def index(request):
     if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            query_array = form.cleaned_data['query'].split(" ")
-            return render(request, "index.html", {'form': form})
+        query = SearchForm(request.POST)
+        image = SearchForImageForm(request.POST, request.FILES)
+        if query.is_valid() and query.cleaned_data['query']:
+            query_array = query.cleaned_data['query'].split(" ")
+            # use the query_array to search by tags and pass them as 'results'
+            query = SearchForm()
+            return render(request, "index.html", {'form': query, 'image_form': image, 'results': {}})
+        elif image.is_valid():
+            image = image.cleaned_data["image"]
+            # use the image to process image and look for similar images in model
+            image = SearchForImageForm()
+            return render(request, "index.html", {'form': query, 'image_form': image, 'results' : {}})
+        else:
+            form = SearchForm()
+            image = SearchForImageForm()
+            return render(request, 'index.html', {'form': form, 'image_form': image, 'error': True})
     else:
         form = SearchForm()
-        return render(request, 'index.html', {'form': form})
+        image = SearchForImageForm()
+        return render(request, 'index.html', {'form': form, 'image_form': image, 'error': False})
