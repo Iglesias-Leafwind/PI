0b42355eab48a7d4d131556a3825d1a846708b9f
Mariana Santos <marianasps@ua.pt>
2021-05-05 23:00:18 +0100

    atualizacao object extraction

--- project/app/object_extraction.py
+++ project/app/object_extraction.py
@@ -1,16 +1,19 @@
-"""
-from imageai.Detection import ObjectDetection
-import os
+import torch
+# https://github.com/ultralytics/yolov5/issues/36
 
-class ObjectExtraction:
+class ObjectExtract():
     def __init__(self):
-        self.detector = ObjectDetection()
-        self.detector.setMOdelTypeAsRetinaNet()
-        self.detector.setModelPath(...)
-        self.detector.loadModel()
+        # exige que o utilizador esteja ligado à net da primeira vez (ele faz uns downloads)
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-    def detect(self, img_path):
-        detections = self.detector.detectObjectsFromImage(input_image=img_path)
-        print(detections)
+        """ # exemplo de como se faz pra n ter de tar ligado à net
+        path = '/some/local/path/pytorch/vision'
+        model = torch.hub.load(path, 'resnet50', pretrained=True)
+        """
 
-"""
+    def get_objects(self, image_path):
+        results = self.model(image_path)
+
+        res = results.pandas().xyxy[0][['confidence', 'name']]
+        print(res)
+        return res
