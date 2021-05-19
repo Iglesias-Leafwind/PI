273e2b196447c199f3a9a9c2e1e6ac215fd30768
Anth0nyPereira <anthonypereira@ua.pt>
2021-05-19 15:02:35 +0100

    Tried to change lists to sets but the overall performance is not getting too much better, oh damn

--- project/app/nlpFilterSearch.py
+++ project/app/nlpFilterSearch.py
@@ -5,24 +5,26 @@ from nltk.tag import pos_tag
 from nltk.stem import WordNetLemmatizer
 import string
 import enchant
+import time
+
 
 def tokenizeText(text):
     word_tokens = word_tokenize(text)
-    return word_tokens
+    return set(word_tokens)
 
 def filterPunctuation(word_tokens):
     filtered_word_tokens = [w for w in word_tokens if w not in string.punctuation]
-    return filtered_word_tokens
+    return set(filtered_word_tokens)
 
 def filterStopWords(filtered_word_tokens):
     stop_words = set(stopwords.words('english'))
     filtered_word_tokens_no_stop_words = [w for w in filtered_word_tokens if w not in stop_words]
-    return filtered_word_tokens_no_stop_words
+    return set(filtered_word_tokens_no_stop_words)
 
 def filteredDictWords(filtered_word_tokens_no_stop_words):
     english_vocab = set(w.lower() for w in words.words())
     real_word_tokens = [w for w in filtered_word_tokens_no_stop_words if w in english_vocab]
-    return real_word_tokens
+    return set(real_word_tokens)
 
 
 def stemmingMethod(real_word_tokens):
@@ -30,7 +32,7 @@ def stemmingMethod(real_word_tokens):
     ls = LancasterStemmer()
 
     # 1st, let's check the PorterStemmer
-    stemmed_words = []
+    stemmed_words = set()
     d = enchant.Dict("en_UK")
     for word in real_word_tokens:
         stemmed_word = ps.stem(word)
@@ -38,25 +40,25 @@ def stemmingMethod(real_word_tokens):
             stemmed_word = ls.stem(word)    # 2nd, let's try the LancasterStemmer
             if stemmed_word not in words.words() or not d.check(stemmed_word):
                 stemmed_word = word
-        stemmed_words += [stemmed_word]
+        stemmed_words |= {stemmed_word}
     
     return list(set(stemmed_words))
 
 def posTagging(stemmed_words):
     words_with_tags = pos_tag(stemmed_words)
-    return words_with_tags
+    return set(words_with_tags)
 
 def transformTagging(words_with_tags):
-    words_with_tags_ = []
+    words_with_tags_ = set()
     for tuple_ in words_with_tags:
         if tuple_[1].startswith("N"):
-            words_with_tags_ += [(tuple_[0], wordnet.NOUN)]
+            words_with_tags_ |= {(tuple_[0], wordnet.NOUN)}
         elif tuple_[1].startswith("V"):
-            words_with_tags_ += [(tuple_[0], wordnet.VERB)]
+            words_with_tags_ |= {(tuple_[0], wordnet.VERB)}
         elif tuple_[1].startswith("A"):
-            words_with_tags_ += [(tuple_[0], wordnet.ADJ)]
+            words_with_tags_ |= {(tuple_[0], wordnet.ADJ)}
         elif tuple_[1].startswith("R"):
-            words_with_tags_ += [(tuple_[0], wordnet.ADV)]
+            words_with_tags_ |= {(tuple_[0], wordnet.ADV)}
         else:
             continue
 
@@ -64,29 +66,34 @@ def transformTagging(words_with_tags):
 
 def lemmatizationMethod(words_with_tags_):
     lemmatizer = WordNetLemmatizer()
-    lemmatized_words = []
+    lemmatized_words = set()
     for tuple_ in words_with_tags_:
         word = tuple_[0]
         pos = tuple_[1]
         lemmatized_word = lemmatizer.lemmatize(word, pos)
-        lemmatized_words += [lemmatized_word]
+        lemmatized_words |= {lemmatized_word}
     
-    return list(set(lemmatized_words))
+    return set(lemmatized_words)
 
 
 def processQuery(query):
     text = query.lower()
-    word_tokens = tokenizeText(text)
-    filtered_word_tokens = filterPunctuation(word_tokens)
-    filtered_word_tokens_no_stop_words = filterStopWords(filtered_word_tokens)
-    stemmed_words = stemmingMethod(filtered_word_tokens_no_stop_words)
-    words_with_tags = posTagging(stemmed_words)
-    words_with_tags_ = transformTagging(words_with_tags)
-    lemmatized_words = lemmatizationMethod(words_with_tags_)
+    results = tokenizeText(text)
+    results = filterPunctuation(results)
+    results = filterStopWords(results)
+    results = stemmingMethod(results)
+    results = posTagging(results)
+    results = transformTagging(results)
+    results = lemmatizationMethod(results)
 
-    return lemmatized_words
+    return results
 
-    
+
+text = "loving someone is something beautiful, just like the nature. I love the world."
+beginning = time.time()
+print(processQuery(text))
+end = time.time()
+print(end-beginning)
 
 
 
