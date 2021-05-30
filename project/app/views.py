817ad34c5678e7700ba3352fa79a2ceb9c885b2c
Alexa <alexandracarvalho@ua.pt>
2021-05-30 01:24:08 +0100

    Finished top stats card row

--- project/app/views.py
+++ project/app/views.py
@@ -7,13 +7,14 @@ import cv2
 from django.shortcuts import render, redirect
 from elasticsearch_dsl import Index, Search, Q
 from app.forms import SearchForm, SearchForImageForm, EditFoldersForm, PersonsForm, PeopleFilterForm
-from app.models import ImageES, ImageNeo, Tag, Person
+from app.models import ImageES, ImageNeo, Tag, Person, Location
 from app.processing import getOCR, getExif, dhash, findSimilarImages, uploadImages, fs, deleteFolder, frr
 from app.utils import addTag, deleteTag, addTagWithOldTag
 from manage import es
 from app.nlpFilterSearch import processQuery
 import re
 import itertools
+
 
 def updateTags(request, hash):
     newTagsString = request.POST.get("tagsTextarea")
@@ -24,15 +25,13 @@ def updateTags(request, hash):
     oldTags = [x.name for x in image.tag.all()]
     print(oldTags)
 
-    for indx,tag in enumerate(newTags):
+    for indx, tag in enumerate(newTags):
         if tag not in oldTags:
             addTag(hash, tag)
 
     for tag in oldTags:
         if tag not in newTags:
             deleteTag(hash, tag)
-
-
 
     query = SearchForm()
     image = SearchForImageForm()
@@ -46,7 +45,9 @@ def updateTags(request, hash):
 
     return render(request, "index.html", {'form': query, 'image_form': image, 'results': results, 'error': False})
 
+
 from app.utils import showDict
+
 
 def index(request):
     if request.method == 'POST':  # if it's a POST, we know it's from search by image
@@ -61,7 +62,8 @@ def index(request):
                 getresult = ImageNeo.nodes.get_or_none(hash=i)  # fetching each corresponding node
                 if getresult:  # if it exists
                     results["results"].append((getresult, getresult.tag.all()))  # append the node and its tags
-            return render(request, "index.html", {'form': query, 'image_form': image, 'results': results, 'error': False})
+            return render(request, "index.html",
+                          {'form': query, 'image_form': image, 'results': results, 'error': False})
         else:  # the form filled had a mistake
             results = {}  # blank results dictionary
             for tag in Tag.nodes.all():  # looping through all tag nodes
@@ -70,25 +72,28 @@ def index(request):
                 for lstImage in results["#" + tag.name]:  # for each image of the value of this tag
                     results["#" + tag.name][count] = (lstImage, lstImage.tag.all())  # replace it by a tuple w/ its tags
                     count += 1  # increase counter
-            return render(request, 'index.html', {'form': query, 'image_form': image, 'results': results, 'error': True})
+            return render(request, 'index.html',
+                          {'form': query, 'image_form': image, 'results': results, 'error': True})
     else:
         if 'query' in request.GET:
-            query = SearchForm()    # cleaning this form
-            image = SearchForImageForm()    # fetching the images form
-            query_text = request.GET.get("query")   # fetching the inputted query
+            query = SearchForm()  # cleaning this form
+            image = SearchForImageForm()  # fetching the images form
+            query_text = request.GET.get("query")  # fetching the inputted query
             query_array = processQuery(query_text)  # processing query with nlp
             tag = "#" + " #".join(query_array)  # arranging tags with '#' before
 
-            result_hashs = list(map(lambda x: x.hash, search(query_array))) # searching and getting result's images hash
-            results = {tag: []} # blank results dictionary
-            for hash in result_hashs:   # iterating through the result's hashes
-                img = ImageNeo.nodes.get_or_none(hash=hash) # fetching the reuslts nodes from DB
-                if img is None: # if there is no image with this hash in DB
-                    continue    # ignore, advance
-                tags = img.tag.all()    # get all tags from the image
-                results[tag].append((img, tags))    # insert tags in the dictionary
+            result_hashs = list(
+                map(lambda x: x.hash, search(query_array)))  # searching and getting result's images hash
+            results = {tag: []}  # blank results dictionary
+            for hash in result_hashs:  # iterating through the result's hashes
+                img = ImageNeo.nodes.get_or_none(hash=hash)  # fetching the reuslts nodes from DB
+                if img is None:  # if there is no image with this hash in DB
+                    continue  # ignore, advance
+                tags = img.tag.all()  # get all tags from the image
+                results[tag].append((img, tags))  # insert tags in the dictionary
 
-            return render(request, "index.html", {'form': query, 'image_form': image, 'results': results, 'error': False})
+            return render(request, "index.html",
+                          {'form': query, 'image_form': image, 'results': results, 'error': False})
 
         else:  # first time in the page - no forms filled
             query = SearchForm()
@@ -101,7 +106,9 @@ def index(request):
                 for lstImage in results["#" + tag.name]:
                     results["#" + tag.name][count] = (lstImage, lstImage.tag.all())
                     count += 1
-            return render(request, 'index.html', {'form': query, 'image_form': image, 'results': results, 'error': False})
+            return render(request, 'index.html',
+                          {'form': query, 'image_form': image, 'results': results, 'error': False})
+
 
 def delete(request, path):
     form = SearchForm()
@@ -109,7 +116,8 @@ def delete(request, path):
     pathf = EditFoldersForm()
     deleteFolder(path)
     folders = fs.getAllUris()
-    return render(request, 'managefolders.html', {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
+    return render(request, 'managefolders.html',
+                  {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
 
 
 def managefolders(request):
@@ -119,17 +127,18 @@ def managefolders(request):
         image = SearchForImageForm()
         pathf = EditFoldersForm()
         folders = fs.getAllUris()
-        return render(request, 'managefolders.html', {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
+        return render(request, 'managefolders.html',
+                      {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
     else:
         form = SearchForm()
         image = SearchForImageForm()
         pathf = EditFoldersForm()
         folders = fs.getAllUris()
-        return render(request, 'managefolders.html', {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
+        return render(request, 'managefolders.html',
+                      {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
 
 
 def managepeople(request):
-
     if request.method == 'POST':
         filters = PeopleFilterForm(request.POST)
         print('entrou aqui...')
@@ -159,7 +168,8 @@ def managepeople(request):
     image = SearchForImageForm()
     names = PersonsForm()
     filters = PeopleFilterForm(initial=showDict)
-    return render(request, 'renaming.html', {'form': form, 'image_form': image, 'names_form': names, 'filters' : filters})
+    return render(request, 'renaming.html',
+                  {'form': form, 'image_form': image, 'names_form': names, 'filters': filters})
 
 
 def search(tags):
@@ -191,6 +201,7 @@ def searchtag(request):
         print(i)
     return render(request, 'index.html')
 
+
 def updateFolders(request):
     folders = fs.getAllUris()
     for folder in folders:
@@ -198,7 +209,9 @@ def updateFolders(request):
     form = SearchForm()
     image = SearchForImageForm()
     pathf = EditFoldersForm()
-    return render(request, 'managefolders.html', {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
+    return render(request, 'managefolders.html',
+                  {'form': form, 'image_form': image, 'folders': folders, 'path_form': pathf})
+
 
 def update_faces(request):
     if request.method != 'POST':
@@ -211,7 +224,7 @@ def update_faces(request):
         # return or smth
 
     if request.POST.get("close"):
-        print('close was called, do something!!') # TODO
+        print('close was called, do something!!')  # TODO
 
     print(form.cleaned_data)
     data = form.cleaned_data
@@ -219,7 +232,7 @@ def update_faces(request):
     imgs = int(len(form.cleaned_data) / 5)
     listt = []
     for i in range(imgs):
-        #if not data['person_verified_%s' % str(i)]:
+        # if not data['person_verified_%s' % str(i)]:
         #    continue
         thumbname = data['person_image_%s' % str(i)]
         new_personname = data['person_name_%s' % str(i)]
@@ -247,12 +260,18 @@ def update_faces(request):
 
     return redirect('/people')
 
+
 def dashboard(request):
     form = SearchForm()
     image = SearchForImageForm()
     person_number = 0
     for p in Person.nodes.all():
-        person_number +=1
+        person_number += 1
+
+    location_number = 0
+    for l in Location.nodes.all():
+        location_number +=1
+
     results = {}
     counts = {}
     for tag in Tag.nodes.all():
@@ -264,16 +283,18 @@ def dashboard(request):
             count += 1
         counts[tag.name] = len(results["#" + tag.name])
 
-    countTags = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True)) # sort the dict by its value (count), from the greatest to the lowest
+    countTags = dict(sorted(counts.items(), key=lambda item: item[1],
+                            reverse=True))  # sort the dict by its value (count), from the greatest to the lowest
     if len(countTags) < 10:
         countTags = dict(itertools.islice(countTags.items(), len(countTags)))
     else:
-        countTags = dict(itertools.islice(countTags.items(), 10)) # only want the first top 10 more common tags
+        countTags = dict(itertools.islice(countTags.items(), 10))  # only want the first top 10 more common tags
     countTags = json.dumps(countTags)
 
     ## original tag source statistics
     countOriginalTagSource = {}
-    allTagLabels = {"ocr":"text from image", "manual":"manual", "object":"objects", "places":"places", "exif":"image properties", "folder":"folder's name", "breeds":"breed"}
+    allTagLabels = {"ocr": "text from image", "manual": "manual", "object": "objects", "places": "places",
+                    "exif": "image properties", "folder": "folder's name", "breeds": "breed"}
     for tag in Tag.nodes.all():
         imgList = tag.image.all()
         for img in imgList:
@@ -292,7 +313,10 @@ def dashboard(request):
             countOriginalTagSource[label] = 0
 
     countOriginalTagSource = dict(sorted(countOriginalTagSource.items(), key=lambda item: item[1], reverse=True))
-    return render(request, 'dashboard.html', {'form': form, 'image_form': image, 'results': results, 'counts': countTags, 'countTagSource': countOriginalTagSource, 'numbers': {'person':person_number}})
+    return render(request, 'dashboard.html',
+                  {'form': form, 'image_form': image, 'results': results, 'counts': countTags,
+                   'countTagSource': countOriginalTagSource, 'numbers': {'person': person_number, 'location': location_number}})
+
 
 def calendarGallery(request):
     form = SearchForm()
@@ -326,7 +350,5 @@ def calendarGallery(request):
     datesInsertion = json.dumps(datesInsertion)
     datesCreation = json.dumps(datesCreation)
     print(datesCreation)
-    return render(request, 'gallery.html', {'form': form, 'image_form': image, 'datesInsertion': datesInsertion, 'datesCreation': datesCreation})
-
-
-
+    return render(request, 'gallery.html',
+                  {'form': form, 'image_form': image, 'datesInsertion': datesInsertion, 'datesCreation': datesCreation})
